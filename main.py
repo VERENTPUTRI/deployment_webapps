5a81bf26aec3853b34343e925ab0a9e01f79d6d5
VERENTPUTRI <verent9@gmail.com>
2023-04-06 19:47:49 -0800

    initial deployment

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title('Aplikasi perhitungan molaritas')
+
+bobot = st.number_input('Masukkan nilai bobot')
+volume = st.number_input('Masukkan nilai volume')
+mr = st.number_input('Masukkan nilai Mr')
+
+tombol = st.button('Hitung nilai molaritas')
+
+if tombol:
+    nilai molaritas = bobot/(mr*volume)
+    st.success(f'Nilai molaritas adalah {nilai_molaritas}')
+    
